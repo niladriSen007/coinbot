ddee6fdf24b1a575e69c927da7e4c8bf3a249d6c
Niladri Sen <109987344+niladriSen007@users.noreply.github.com>
2022-11-02 16:24:43 +0530

    Add files via upload

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,15 @@
-import React from 'react'
-import Button from './Button';
-import styled from "styled-components"
+import React, { useState } from "react";
+import styled from "styled-components";
+import { GiHamburgerMenu } from "react-icons/gi";
+import { MdClose } from "react-icons/md";
+import Button from "./Button";
+
 const Navbar = () => {
+
+    const [isNavOpen,setIsNavOpen] = useState(false);
+    const html = document.querySelector("html")
+    html.addEventListener("click",()=>setIsNavOpen(false))
+
     // const links = ["Home","Dashboard","Learn","FAQ","Contact","About"];
     const links = [
         {
@@ -24,12 +32,23 @@ const Navbar = () => {
                          }
 ]
   return (
-    <Nav className='flex j-between a-center'>
+    <Nav state={isNavOpen ? 1 :0} className='flex j-between a-center sticky'>
         <div className="brand">
             <h2>CoinBot</h2>
         </div>
-        <div className="toggle"></div>
-        <div className="links">
+        <div className="toggle">
+            {
+                isNavOpen
+                 ? 
+                <MdClose onClick={()=>setIsNavOpen(false)} /> 
+                :
+                 <GiHamburgerMenu onClick={(e)=>{
+                    e.stopPropagation();
+                    setIsNavOpen(true)
+                }}/>
+            }
+        </div>
+        <div className={`links ${isNavOpen ? "show" : ""}`}>
             <ul className='flex gap-2'>
                 {
                     links.map(({name,link})=>{
@@ -50,43 +69,90 @@ const Navbar = () => {
   )
 }
 
-const Nav = styled.div`
-margin:3rem 5rem;
-.brand
-{
-    h2 {
-        font-size: 2.5rem;
-      }
+const Nav = styled.nav`
+margin: 3rem 5rem;
+.brand {
+  h2 {
+    font-size: 2.5rem;
+  }
 }
-.toggle
-{
-    display:none;
+.toggle {
+  display: none;
 }
-.links
-{
-    ul{
-        font-weight:bolder;
-        li{
-            cursor:pointer;
-            &:first-child
-            {
-                a{
-                    opacity:1;
-                }
-            }
-            a
-            {
-                opacity:0.7;
-                transition:0.5s ease-in-out;
-                font-size:1.15rem;
-                &:hover
-                {
-                    opacity:1;
-                }
-            }
-
+.links {
+  ul {
+    font-weight: bolder;
+    li {
+      cursor: pointer;
+      &:first-of-type {
+        a {
+          opacity: 1;
         }
+      }
+      a {
+        opacity: 0.7;
+        transition: 0.5s ease-in-out;
+        &:hover {
+          opacity: initial;
+        }
+      }
     }
+  }
+}
+@media screen and (min-width: 280px) and (max-width: 1080px) {
+  padding: 3rem 1rem;
+  margin: 0;
+  position: relative;
+  z-index: 5;
+  flex-direction: row;
+  .brand {
+    h2 {
+      font-size: 2rem;
+    }
+  }
+  .toggle {
+    padding-right: 1rem;
+    display: block;
+    z-index: 51;
+    svg {
+      color: #2d69fd;
+    }
+  }
+  .show {
+    opacity: 1 !important;
+    // transform:translateX(-90rem);
+    // z-index:100;
+    visibility: visible !important;
+  }
+  .links {
+    z-index: 10;
+    position: fixed;
+    overflow-x: hidden;
+    top: 0;
+    right: 0;
+    width: ${({ state }) => (state ? "60%" : "0%")};
+    height: 120vh;
+    background-color: white;
+    opacity: 0;
+    visibility: hidden;
+    transition: 0.7s ease-in-out;
+    ul {
+      flex-direction: column;
+      display:flex;
+      text-align: center;
+      height: 100vh;
+    //   align-items:center;
+      justify-content: center;
+      li {
+        a {
+          color: #2d69fd;
+        }
+      }
+    }
+  }
+  .auth {
+    display: none;
+  }
 }
 
 `;
